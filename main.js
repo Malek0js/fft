715f519e803ea58757e5eac5b9439bdb5866664f
Malek0js <121379844+Malek0js@users.noreply.github.com>
2022-12-27 08:53:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 import * as THREE from 'three';
-import { MeshBasicMaterial } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { MeshStandardMaterial, Vector3 } from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader.js'
+import { ImageLoader } from 'three';
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let af = false;
+let awayForward = false;
+let awayBackward = false;
+let awayRight = false;
+let awayLeft = false;
+let adds = 3;
+
+
 
 class BasicScene {
   constructor(){
@@ -20,9 +36,11 @@ class BasicScene {
     }, false);
 
     this.camera = new THREE.PerspectiveCamera(60, 1920 / 1080, 1.0, 1000.0);
-    this.camera.position.z = 100;
+    this.camera.position.z = 70;
 
     this.scene = new THREE.Scene();
+
+    this.OnWindowResize();
 
     let light = new THREE.DirectionalLight(0xFFFFFF);
     light.position.set(100,100,100);
@@ -31,30 +49,121 @@ class BasicScene {
 
     this.scene.add(light);
 
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
     light = new THREE.AmbientLight(0x404040);
     this.scene.add(light);
 
-    const controls = new OrbitControls( this.camera, this.renderer.domElement );
-    controls.update();
+    window.addEventListener('keydown', (e)=>{
+      if(e.code == 'KeyW'){
+        moveForward = true;
+      }
+    })
+    window.addEventListener('keydown', (e)=>{
+      if(e.code == 'KeyA'){
+        moveLeft = true;
+      }
+    })
+    window.addEventListener('keydown', (e)=>{
+      if(e.code == 'KeyS'){
+        moveBackward = true;
+      }
+    })
+    window.addEventListener('keydown', (e)=>{
+      if(e.code == 'KeyD'){
+        moveRight = true;
+      }
+    })
+
+    window.addEventListener('keyup', (e)=>{
+      if(e.code == 'KeyW'){
+        moveForward = false;
+      }
+    })
+    window.addEventListener('keyup', (e)=>{
+      if(e.code == 'KeyA'){
+        moveLeft = false;
+      }
+    })
+    window.addEventListener('keyup', (e)=>{
+      if(e.code == 'KeyS'){
+        moveBackward = false;
+      }
+    })
+    window.addEventListener('keyup', (e)=>{
+      if(e.code == 'KeyD'){
+        moveRight = false;
+      }
+    })
+    
+    this.scene.background = new THREE.CubeTextureLoader()
+  	.setPath( './clouds1/' )
+  	.load( [
+	  	'clouds1_east.bmp',
+      'clouds1_west.bmp',
+	  	'clouds1_up.bmp',
+	  	'clouds1_down.bmp',
+	  	'clouds1_north.bmp',
+      'clouds1_south.bmp'
+	  ] );
 
 
-    this.scene.background = new THREE.Color(0x2C3E50)
+    this.players = [];
 
-    const plane = new THREE.Mesh(
-      new THREE.PlaneGeometry(100,100,1,1),
-      new MeshBasicMaterial({color:0x808080, side: THREE.DoubleSide})
-    );
-    this.scene.add(plane);
+    this.makeplayer('malek');
+    this.LoadAnimatedModel();
+
     this.RAF();
+    }
+  LoadAnimatedModel(){
+    const loader = new FBXLoader();
+    loader.setPath("./");
+    loader.load('Mremireh_O_Desbiens.fbx', (fbx) => {
+      fbx.scale.setScalar(0.1);
+      fbx.traverse(c => {
+        c.castShadow = true;
+      });
+      this.scene.add(fbx);
+    });
+
   }
   OnWindowResize(){
   this.camera.aspect = window.innerWidth / window.innerHeight;
   this.camera.updateProjectionMatrix();
   this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
+  makeplayer(id){
+    this.players.push(id);
+    let player = new THREE.Mesh(
+      new THREE.BoxGeometry(100,100,.09),
+      new MeshStandardMaterial({color:0x808080})
+    );
+    this.scene.add(player);
+    player.rotation.x = 1.6;
+  }
+
   RAF(){
     requestAnimationFrame(() => {
+
       this.renderer.render(this.scene, this.camera);
+
+      if(moveForward){
+       // this.scene.children[2].position.y += .09;
+      }
+      if(moveBackward){
+        //this.scene.children[2].position.y -= .09;
+      }
+      if(moveLeft){
+        //this.scene.children[2].position.x -= .09;
+      }
+      if(moveRight){
+       // this.scene.children[2].position.x += .09;
+      }
+    
+      //this.camera.position.set(this.scene.children[2].position.x,this.scene.children[2].position.y,10);
+
+      this.controls.update();
+
       this.RAF();
     });
   }
